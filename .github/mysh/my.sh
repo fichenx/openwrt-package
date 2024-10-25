2bea7b6b237ae75c0881b448845b014ead8385f4
尘事尘飞 <86181542+fichenx@users.noreply.github.com>
2024-10-25 21:41:02 +0800

    Update my.sh

--- .github/mysh/my.sh
+++ .github/mysh/my.sh
@@ -93,6 +93,11 @@ git clone -b master https://github.com/MilesPoupart/luci-app-vssr luci-app-vssr
 rm -rf luci-app-socat
 git_svn main https://github.com/chenmozhijin/luci-app-socat luci-app-socat
 
+#####luci-app-mosdns mosdns v2dat#####
+rm -rf luci-app-mosdns mosdns v2dat
+git_svn v5_lua https://github.com/sbwml/luci-app-mosdns luci-app-mosdns mosdns v2dat 
+
+
 ############暂时替换原kenzok8/small-package/.github/diy/main.sh中无法使用的svn命令############
 git_sparse_clone master "https://github.com/immortalwrt/luci" "temp" applications/luci-app-homeproxy
 git_sparse_clone master "https://github.com/coolsnowwolf/luci" "temp" libs/luci-lib-ipkg
