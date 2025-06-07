d04c38d7f6438ab94fe81a1cb59e0c677ebde75e
fichenx@gmail.com <fichenx@gmail.com>
2025-06-07 11:16:19 +0800

    fix

--- .github/mysh/edit_main.sh
+++ .github/mysh/edit_main.sh
@@ -11,6 +11,6 @@ sed -i "s|git clone --depth 1 https://github.com/kenzok8/wall|#git clone --depth
 sed -i "s|git clone --depth 1 https://github.com/sirpdboy/netspeedtest|#git clone --depth 1 https://github.com/sirpdboy/netspeedtest|g" .github/diy/main.sh
 
 #修复diy/main.sh中格式错误
-sed -i "s|applications/luci-app-watchcat \|applications/luci-app-watchcat|g" .github/diy/main.sh
-sed -i "s|luci-app-socat \|luci-app-socat|g" .github/diy/main.sh
+sed -i "s|'applications/luci-app-watchcat \'|'applications/luci-app-watchcat'|g" .github/diy/main.sh
+sed -i "s|'luci-app-socat \'|'luci-app-socat'|g" .github/diy/main.sh
 
