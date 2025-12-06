6874af6570ee44304bcff0846b887f0c244aeca2
尘事尘飞 <86181542+fichenx@users.noreply.github.com>
2025-12-06 20:40:50 +0800

    更新docker版本至28.5.2并更新依赖
    Updated docker and its dependencies with new versions and hashes.

--- .github/mysh/my.sh
+++ .github/mysh/my.sh
@@ -192,14 +192,23 @@ sed -i 's|../../luci.mk|$(TOPDIR)/feeds/luci/luci.mk|g' luci-app-nft-qos/Makefil
 sed -i 's|../../luci.mk|$(TOPDIR)/feeds/luci/luci.mk|g' luci-app-wireguard/Makefile
 sed -i 's|../../luci.mk|$(TOPDIR)/feeds/luci/luci.mk|g' luci-lib-ipkg/Makefile
 
-#更新dockerd(修改前使用的源为：https://github.com/kenzok8/wall)
-sed -i 's|\(PKG_VERSION:=\)[0-9]\+\.[0-9]\+\.[0-9]\+|\129.1.2|g' dockerd/Makefile
-sed -i 's|PKG_HASH:=.*|PKG_HASH:=f11949f3d8dcbcbd6d5de0b345b5ec8dbdd74572c6ce614c7b6e7551829d8035|g' dockerd/Makefile
-sed -i 's|PKG_GIT_REF:=v|PKG_GIT_REF:=docker-v|g' dockerd/Makefile
-#更新docker(修改前使用的源为：https://github.com/kenzok8/wall)
-#sed -i 's|\(PKG_VERSION:=\)[0-9]\+\.[0-9]\+\.[0-9]\+|\128.3.0|g' docker/Makefile
-#sed -i 's|PKG_HASH:=.*|PKG_HASH:=0ac18927138cd2582e02277d365174a118b962f10084a6bef500a58de705e094|g' docker/Makefile
-
+##docker及其依赖
+#dockerd(修改前使用的源为：https://github.com/kenzok8/wall)
+sed -i 's|\(PKG_VERSION:=\)[0-9]\+\.[0-9]\+\.[0-9]\+|\128.5.2|g' dockerd/Makefile
+sed -i 's|PKG_HASH:=.*|PKG_HASH:=0e450c03c536a1304ba8fd26ca4c4ff96fac62182fd042fec90ffdf4a0969d40|g' dockerd/Makefile
+#sed -i 's|PKG_GIT_REF:=v|PKG_GIT_REF:=docker-v|g' dockerd/Makefile
+#docker(修改前使用的源为：https://github.com/kenzok8/wall)
+sed -i 's|\(PKG_VERSION:=\)[0-9]\+\.[0-9]\+\.[0-9]\+|\128.5.2|g' docker/Makefile
+sed -i 's|PKG_HASH:=.*|PKG_HASH:=5864ccdfceef3a9e8ddb29b4a555d5b82a551b5258067aac164b6156c6cfdc08|g' docker/Makefile
+#containerd
+git_sparse_clone master https://github.com/coolsnowwolf/packages utils/containerd
+sed -i 's/PKG_VERSION:=.*/PKG_VERSION:=1.7.29/g' containerd/Makefile
+sed -i 's/PKG_HASH:=.*/PKG_HASH:=e9c467a96b2d7f5b5348c090da75d80ed1889ff6d0f649f87efdf5c311a02500/g' containerd/Makefile
+sed -i 's/containerd-shim,containerd-shim-runc-v1,//g' containerd/Makefile
+#runc
+git_sparse_clone master https://github.com/coolsnowwolf/packages utils/runc
+sed -i 's/PKG_VERSION:=.*/PKG_VERSION:=1.3.3/g' runc/Makefile
+sed -i 's/PKG_HASH:=.*/PKG_HASH:=3da010af5c2c1c3d12d460255e4a7dc049c223ebc629c80fbbe4c10720997ffe/g' runc/Makefile
 
 
 ######################################
